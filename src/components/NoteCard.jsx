16bcabfc5e06b7494219ae4125c04dce1597ff16
Miguel Fung <64858239+mjfung1@users.noreply.github.com>
2023-03-14 04:43:36 -0400

    Update NoteCard.jsx

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -36,7 +36,7 @@ function NoteCard({ note, handleDelete }) {
                 <CardHeader 
                     avatar={
                         <Avatar 
-                            src={`./../avatars/${note.person}.jpeg`}
+                            src={`./../the-office/avatars/${note.person}.jpeg`}
                         />
                         
                     }
